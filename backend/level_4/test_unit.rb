a0f9e5fb09be681061eca740b44029776b46490d
Soul Physician <shinta42@hotmail.fr>
2020-10-07 21:51:18 +0200

    add test unity

--- backend/level_4/test_unit.rb
+++ backend/level_4/test_unit.rb
@@ -1,4 +1,6 @@
 require './json_to_hash.rb'
+require './rental.rb'
+require './car.rb'
 require "test/unit"
 
 class TestUnit < Test::Unit::TestCase
@@ -8,6 +10,17 @@ class TestUnit < Test::Unit::TestCase
     assert_equal("Errno::ENOENT", JsonToHash.read_file("bad_input.json").class.name)
   end
 
-  def test_rental
+  def test_rental_price
+    rental = Rental.new(1, Car.new(1, 2000, 10), "2015-03-31", "2015-04-01", 300)
+
+    assert_equal(6800, rental.driver.amount)
+    assert_equal(4760, rental.owner.amount)
+  end
+
+  def test_rental_commission
+    rental = Rental.new(1, Car.new(1, 2000, 10), "2015-03-31", "2015-04-01", 300)
+    assert_equal(1020, rental.commission.insurance_fee)
+    assert_equal(200, rental.commission.assistance_fee)
+    assert_equal(820, rental.commission.drivy_fee)
   end
 end
