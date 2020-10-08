f80c98e2ddfc5cee2552d8c978176b9a627d877e
Soul Physician <shinta42@hotmail.fr>
2020-10-08 11:45:06 +0200

    add test unity

---
+++ front_end/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      </nav>
+    </div>
+  );
+}
+
+export default App;
